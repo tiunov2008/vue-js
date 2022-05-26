d74627b437f80cc84e264a71e89674535bc65d8f
Tiunov Vladimir <tiunov2008.va@gmail.com>
2022-05-26 12:03:11 +0300

    lesson-2

--- lesson-1/coffee-shop/src/App.vue
+++ lesson-1/coffee-shop/src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="app">
-    Hello Vue.JS
-  </div>
+    <div class="wrapper">
+        <hero-view/>
+        <footer-component/>
+    </div>
 </template>
 
 <script>
+import HeroView from '@/views/HeroView.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
 export default {
+    components: {HeroView, FooterComponent },
 }
 </script>
 
